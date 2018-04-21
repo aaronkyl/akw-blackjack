6ea68367b6e9e00c96a84b881e7dd7a49fda683b
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-04-21 03:22:15 +0000

    add card images and deal/hit/card logic

--- script/blackjack.js
+++ script/blackjack.js
@@ -4,13 +4,13 @@ function Card(suit, name, value) {
     this.value = value;
     
     this.drawCardImage = function() {
-        return '<img src="${this.name}_of_${this.suit}.png" alt="${this.name} of ${this.suit}">';
+        return `<img class="card" src="img/${this.name}_of_${this.suit}.png" alt="${this.name} of ${this.suit}">`;
     };
 }
 
 function Deck() {
     this.suits = ["hearts", "spades", "diamonds", "clubs"];
-    this.names = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    this.names = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
     this.cards = [];
     var that = this;
     
@@ -30,27 +30,45 @@ function Deck() {
             that.cards[card1Index] = that.cards[card2Index];
             that.cards[card2Index] = temp;
         }
+        return;
     };
 }
 
-function Hand(id, deck) {
+function Hand(id) {
     this.id = id;
     this.cards = [];
     var that = this;
     
     this.drawCard = function(deck) {
-        let cardIndex = Math.floor(Math.random() * 53);
-        let card = deck.cards[cardIndex];
-        deck.cards.splice(cardIndex, 1);
-        that.cards.append(card);
+        let cardIndex = Math.floor(Math.random() * deck.cards.length);
+        let card = deck.cards.splice(cardIndex, 1)[0];
+        that.cards.push(card);
+        return card;
     };
 }
 
 $(document).ready(function() {
+    // initialize game
     var deck = new Deck();
-    var dealer = new Hand("dealer", deck);
-    var player = new Hand("player", deck);
+    deck.shuffle();
+    var dealer = new Hand("dealer");
+    var player = new Hand("player");
     
-    console.log(dealer);
-    console.log(player);
+    // clear hands and deal a hand of two cards each to player and dealer
+    $('#deal-button').click(function() {
+        $(".hand").empty();
+        [player, dealer].forEach(function(e) {
+            e.cards = [];
+            for(var i = 0; i < 2; i++) {
+                let card = e.drawCard(deck);
+                $(`#${e.id}-hand`).append(card.drawCardImage());
+            }
+            console.log(e.cards);
+        });
+    });
+    
+    // hit - deal one card to player
+    $('#hit-button').click(function() {
+        player.drawCard(deck);
+    });
 })
